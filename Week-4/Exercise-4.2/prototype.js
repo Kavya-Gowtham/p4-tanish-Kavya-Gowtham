5ee69a2764d1ced86a7e3d5569b7a53e98985197
KavyaNagaraj <kavyanagaraj20596@gmail.com>
2022-08-13 23:05:00 +0530

    Updated changes for  prototype inheritence

--- Week-4/Exercise-4.2/prototype.js
+++ Week-4/Exercise-4.2/prototype.js
@@ -3,14 +3,16 @@ const Person = function () {};
 Person.prototype.initialize = function (name, age) {
   this.name = name;
   this.age = age;
-  this.teach = function(subject) {
-      console.log(this.name + " is teaching " + subject);
-      console.log(this.name + " is of age " + this.age);
-  }
 };
+function Teacher() {
+}
 
-const Teacher = new Person();
-const person = Teacher;
+Teacher.prototype.teach = function (subject) {
+    console.log(`${this.name} is now teaching ${subject}`);
+}
+Object.setPrototypeOf(Teacher.prototype, Person.prototype)
 
-person.initialize("Adam", 45);
-person.teach("Inheritance");
+var him = new Teacher();
+him.initialize("Adam", 45);
+him.teach("Inheritance");
+
