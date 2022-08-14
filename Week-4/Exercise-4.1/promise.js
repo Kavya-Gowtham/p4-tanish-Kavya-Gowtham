dc115717fae3f4ce59c25927a758023e2a6f18aa
KavyaNagaraj <kavyanagaraj20596@gmail.com>
2022-08-14 22:41:13 +0530

    Added code for custom promise to implement get random number

--- Week-4/Exercise-4.1/promise.js
+++ Week-4/Exercise-4.1/promise.js
@@ -1,18 +1,124 @@
-const getNumber = () => 
-new Promise((resolve, reject) => {
-    const randomNumber = parseInt(Math.random() * 100, 10);
-    console.log("random num = ", randomNumber);
-    setTimeout(() => {
-        if (randomNumber % 5 === 0) {
-            reject(`Rejected with num: ${randomNumber}`);
-        }
-        resolve(`Resolved with num: ${randomNumber}`);
-    }, randomNumber * 10);
-});
-
-const result = (content) => {
-    console.log(content);
+const getNumber = () => {
+    return Math.ceil(Math.random() * 1024);
+}
+const STATE = {
+    PENDING: 'PENDING',
+    FULFILLED: 'FULFILLED',
+    REJECTED: 'REJECTED',
+    RESOLVED: 'RESOLVED'
 }
 
-const numberpromise = getNumber();
-numberpromise.then(result).catch(result);
+function isThenable(val) {
+    return val instanceof MyPromise;
+}
+class MyPromise {
+    constructor(callback) {
+        this.state = STATE.PENDING;
+        this.value = undefined;
+        this.handlers = [];
+        try {
+            callback(this.resolve, this.reject);
+        } catch (err) {
+            this.reject(err)
+        }
+    }
+
+    resolve = (value) => {
+        this.updateResult(value, STATE.RESOLVED);
+    }
+
+    reject = (error) => {
+        this.updateResult(error, STATE.REJECTED);
+    }
+
+    updateResult(value, state) {
+        setTimeout(() => {
+            if (this.state !== STATE.PENDING) {
+                return;
+            }
+            if (isThenable(value)) {
+                return value.then(this.resolve, this.reject);
+            }
+
+            this.value = value;
+            this.state = state;
+
+            this.executeHandlers();
+        }, 0);
+    }
+
+    addHandlers(handlers) {
+        this.handlers.push(handlers);
+        this.executeHandlers();
+    }
+
+    executeHandlers() {
+        if (this.state === STATE.PENDING) {
+            return null;
+        }
+
+        this.handlers.forEach((handler) => {
+            if (this.state === STATE.RESOLVED) {
+                return handler.onSuccess(this.value);
+            }
+            return handler.onFail(this.value);
+        });
+        this.handlers = [];
+    }
+
+    then(onSuccess, onFail) {
+        return new MyPromise((res, rej) => {
+            this.addHandlers({
+                onSuccess: function (value) {
+                    if (!onSuccess) {
+                        return res(value);
+                    }
+                    try {
+                        return res(onSuccess(value))
+                    } catch (err) {
+                        return rej(err);
+                    }
+                },
+                onFail: function (value) {
+                    if (!onFail) {
+                        return rej(value);
+                    }
+                    try {
+                        return res(onFail(value))
+                    } catch (err) {
+                        return rej(err);
+                    }
+                }
+            });
+        });
+    }
+    catch(onFail) {
+        return this.then(null, onFail);
+    }
+
+    finally() {
+        console.log(STATE.FULFILLED)
+    }
+}
+
+const test = (time) => new MyPromise((resolve, reject) => {
+    const value = getNumber();
+    console.log("Value : " + value);
+    console.time();
+    setTimeout(() => {
+        if (value % 5 === 0) {
+            reject();
+        }
+        else {
+            resolve();
+        }
+        console.timeEnd();
+    }, time);
+});
+
+test(5000).then(() => {
+    console.log(STATE.RESOLVED);
+}).catch((err) => {
+    debugger
+    console.log(STATE.REJECTED, err);
+})
