1b37460cb8042cbcee37fc224e244da04ddc2f59
KavyaNagaraj <kavyanagaraj20596@gmail.com>
2022-09-25 00:46:46 +0530

    implemented basic structure of todo list

--- Week-9/todo-list/src/App.js
+++ Week-9/todo-list/src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+    <div className="app-wrapper">
+      <div className="header">
+      <h1>Task manager</h1>
+      </div>
+      <div className="main">
+        <h2>Todo-list</h2>
+      </div>
     </div>
+  </div>
   );
 }
 
